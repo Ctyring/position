ae1b28ca5e842439b446983b159ba802ff29e482
Ctyring <75520657+Ctyring@users.noreply.github.com>
2022-08-23 14:19:20 +0800

    fix bug

---
+++ position_parent/position_cty/src/main/java/position/web/Application.java
@@ -0,0 +1,33 @@
+package position.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import position.web.cty.util.IdWorker;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import xyz.erupt.core.annotation.EruptScan;
+
+@SpringBootApplication
+@EnableSwagger2
+@EntityScan
+@EruptScan
+@EnableDiscoveryClient
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public IdWorker idWorkker() {
+        return new IdWorker(1, 1);
+    }
+
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
