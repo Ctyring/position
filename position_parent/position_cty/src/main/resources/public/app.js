99cb6a950fecc3f6f2f3b13e0e4620e88087c4e4
Ctyring <75520657+Ctyring@users.noreply.github.com>
2022-08-24 09:46:02 +0800

    fix bug

--- position_parent/position_cty/src/main/resources/public/app.js
+++ position_parent/position_cty/src/main/resources/public/app.js
@@ -8,7 +8,7 @@ window.eruptSiteConfig = {
   	//描述
     desc: "云职位后台管理系统",
 	  //是否展示版权信息
-	  copyright: true,
+	  copyright: false,
     //高德地图api key,使用地图组件须指定此属性，amapKey获取地址：https://lbs.amap.com (服务平台为：Web端(JS API))
     amapKey: "6ba79a8d",
   	//logo路径
